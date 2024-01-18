254facff37e550142dcd58903691bf38f9cdccb7
Lenovo-User <Lenovo-User@DESKTOP-JS25F1I>
2024-01-18 21:40:40 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnClickMe_Click(sender As Object, e As EventArgs) Handles btnClickMe.Click
+
+        MessageBox.Show("2 + 2 = " & 2 + 2)
+        MessageBox.Show("2 - 2 = " & 2 - 2)
+        MessageBox.Show("2 * 2 = " & 2 * 2)
+        MessageBox.Show("2 / 2 = " & 2 / 2)
+    End Sub
+End Class
